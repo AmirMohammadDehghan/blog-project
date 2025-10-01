cda6da93f91fcd612a538da2096c9b02ad63e749
Amir Mohammad Dehghan <ad9191386@gmail.com>
2025-10-01 09:57:46 +0330

    Add Article model, views, and initial templates ep 14
    Introduces the initial Article model and migration, basic article list view, and placeholder detail/create views. Adds article_list.html template and includes for hero and posts sections to support article listing UI.

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def article_list(request):
+  articles = Article.objects.all().order_by('-updated_at')
+
+  context = {
+    'articles': articles
+  }
+  
+  return render(request, 'articles/article_list.html', context)
+
+def article_detail(request, slug):
+  pass
+
+def article_create(request):
+  pass
